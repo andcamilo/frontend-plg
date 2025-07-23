788c6926fead94a30210c3069834eb44da240516
Felipe F <mundofelipote@gmail.com>
2025-07-23 19:11:35 -0300

    eliminar "use cliente" innecesario

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,6 +1,5 @@
-"use client";
 import React from "react";
-import LegixStatistics from "../../components/legixStadistics";
+import LegixStatistics from "./components/legixStadistics";
 
 export default function Requests() {
   return (
