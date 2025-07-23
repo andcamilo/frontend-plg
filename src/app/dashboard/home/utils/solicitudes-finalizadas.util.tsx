695105fcf86c74f2887c6f575037688c1d662b67
Felipe F <mundofelipote@gmail.com>
2025-07-23 20:10:38 -0300

    Add solicitudesFinalizadas utility and refactor LegixStatistics component to implement pagination for finalized solicitudes

---
+++ src/app/dashboard/home/utils/solicitudes-finalizadas.util.tsx
@@ -0,0 +1,23 @@
+import { getSolicitudesFiltradasPorRol } from "./solicitudes-filtradas-por-rol.util";
+import { TIPO_MAPPING } from "../constants/tipo-mapping.constant";
+import { STATUS_CLASSES } from "../constants/status-classes.constant";
+import { STATUS_MAPPING } from "../constants/status-mapping.constant";
+import { formatDate } from "./format-date.util";
+
+export const solicitudesFinalizadas = (
+  allSolicitudes: any[],
+  formData: any
+) => {
+  return getSolicitudesFiltradasPorRol(allSolicitudes, formData)
+    .filter((solicitud) => parseInt(solicitud.status) === 70)
+    .map(({ tipo, emailSolicita, date, status }) => ({
+      Tipo: TIPO_MAPPING[tipo] || tipo,
+      Fecha: formatDate(date),
+      Email: emailSolicita,
+      Estatus: (
+        <span className={`status-badge ${STATUS_CLASSES[status]}`}>
+          {STATUS_MAPPING[status]}
+        </span>
+      ),
+    }));
+};
