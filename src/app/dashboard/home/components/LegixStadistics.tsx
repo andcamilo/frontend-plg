6864210ecf1dc9e8b40c021d03bd7c5a5db9709e
Felipe F <mundofelipote@gmail.com>
2025-07-23 19:10:59 -0300

    Cambio de lugar de componente LegixStadistics

--- src/app/dashboard/home/components/LegixStadistics.tsx
+++ src/app/dashboard/home/components/LegixStadistics.tsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import HomeBox from '@components/homeBox';
-import FormalitiesChart from '@components/formalitiesChart';
-import TableWithPagination from '@components/TableWithPagination';
-import DashboardCard from '@components/dashboardCard';
-import PivotTable from '@components/pivotTable';
-import { getRequests } from '@api/request';
-import { getRequestsCuenta } from '@api/request-cuenta';
+"use client";
+import React, { useEffect, useState } from "react";
+import HomeBox from "@components/homeBox";
+import FormalitiesChart from "@components/formalitiesChart";
+import TableWithPagination from "@components/TableWithPagination";
+import DashboardCard from "@components/dashboardCard";
+import PivotTable from "@components/pivotTable";
+import { getRequests } from "@api/request";
+import { getRequestsCuenta } from "@/src/app/dashboard/home/services/request-cuenta.service";
 import { checkAuthToken } from "@utils/checkAuthToken";
 import axios from "axios";
-import get from 'lodash/get';
+import get from "lodash/get";
 
 // Function to format date as dd/mm/yyyy
-const formatDate = (timestamp?: { _seconds: number; _nanoseconds: number }): string => {
-  if (!timestamp || typeof timestamp._seconds !== 'number') {
-    console.error('Invalid or missing timestamp:', timestamp);
-    return 'Invalid date'; // Return a fallback message if the timestamp is invalid
+const formatDate = (timestamp?: {
+  _seconds: number;
+  _nanoseconds: number;
+}): string => {
+  if (!timestamp || typeof timestamp._seconds !== "number") {
+    console.error("Invalid or missing timestamp:", timestamp);
+    return "Invalid date"; // Return a fallback message if the timestamp is invalid
   }
 
   const date = new Date(timestamp._seconds * 1000);
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
   const year = date.getFullYear();
 
   return `${day}/${month}/${year}`;
 };
 
-
 const LegixStatistics: React.FC = () => {
   const [solicitudes, setSolicitudes] = useState<any[]>([]);
   const [allSolicitudes, setAllSolicitudes] = useState<any[]>([]);
   const [tipoCounts, setTipoCounts] = useState<{ [key: string]: number }>({});
-  const [statusCounts, setStatusCounts] = useState<{ status10: number; status20: number }>({
+  const [statusCounts, setStatusCounts] = useState<{
+    status10: number;
+    status20: number;
+  }>({
     status10: 0,
     status20: 0,
   });
@@ -82,19 +88,20 @@ const LegixStatistics: React.FC = () => {
       const cuenta = formData.cuenta;
 
       const esCliente =
-        (typeof rol === 'number' && rol < 20) ||
-        rol === 'Cliente' || rol === 'Cliente recurrente';
+        (typeof rol === "number" && rol < 20) ||
+        rol === "Cliente" ||
+        rol === "Cliente recurrente";
 
       const esAbogadoOAsistente =
-        rol === 'Abogados' || rol === 'Asistente' || rol === 40 || rol === 35;
+        rol === "Abogados" || rol === "Asistente" || rol === 40 || rol === 35;
 
       if (esCliente) {
         return solicitud.cuenta === cuenta;
       }
 
       if (esAbogadoOAsistente) {
-        const abogadoAsignado = (solicitud.abogados || []).some((abogado: any) =>
-          abogado?.id === cuenta || abogado?._id === cuenta
+        const abogadoAsignado = (solicitud.abogados || []).some(
+          (abogado: any) => abogado?.id === cuenta || abogado?._id === cuenta
         );
         return solicitud.cuenta === cuenta || abogadoAsignado;
       }
@@ -108,7 +115,7 @@ const LegixStatistics: React.FC = () => {
 
   useEffect(() => {
     const userData = checkAuthToken();
-    console.log("userData ", userData)
+    console.log("userData ", userData);
     if (userData) {
       setFormData((prevData) => ({
         ...prevData,
@@ -124,20 +131,19 @@ const LegixStatistics: React.FC = () => {
     if (formData.cuenta) {
       const fetchUser = async () => {
         try {
-          console.log("Cuenta ", formData.cuenta)
-          const response = await axios.get('/api/get-user-cuenta', {
+          console.log("Cuenta ", formData.cuenta);
+          const response = await axios.get("/api/get-user-cuenta", {
             params: { userCuenta: formData.cuenta },
           });
 
           const user = response.data;
-          console.log("Usuario ", user)
+          console.log("Usuario ", user);
           setFormData((prevData) => ({
             ...prevData,
-            rol: get(user, 'solicitud.rol', 0)
+            rol: get(user, "solicitud.rol", 0),
           }));
-
         } catch (error) {
-          console.error('Failed to fetch solicitudes:', error);
+          console.error("Failed to fetch solicitudes:", error);
         }
       };
 
@@ -145,36 +151,41 @@ const LegixStatistics: React.FC = () => {
     }
   }, [formData.cuenta]);
 
-  const [lastVisibleCursor, setLastVisibleCursor] = useState<string | null>(null);
+  const [lastVisibleCursor, setLastVisibleCursor] = useState<string | null>(
+    null
+  );
   const [hasNextPage, setHasNextPage] = useState(false);
 
   const fetchAllSolicitudes = async () => {
     try {
+      if (
+        (typeof formData.rol === "number" && formData.rol < 20) ||
+        (typeof formData.rol === "string" &&
+          (formData.rol === "Cliente" || formData.rol === "Cliente recurrente"))
+      ) {
+        const solicitudesData = await getRequestsCuenta(
+          rowsPerPage,
+          formData.cuenta,
+          lastVisibleCursor
+        );
 
-      if ((typeof formData.rol === 'number' && formData.rol < 20) ||
-        (typeof formData.rol === 'string' && (formData.rol === 'Cliente' || formData.rol === 'Cliente recurrente'))) {
-
-        const solicitudesData = await getRequestsCuenta(rowsPerPage, formData.cuenta, lastVisibleCursor);
-
-        const {
-          solicitudes = [],
-          tipoCounts,
-        } = solicitudesData;
-        console.log("All Solicituds ", solicitudes)
+        const { solicitudes = [], tipoCounts } = solicitudesData;
+        console.log("All Solicituds ", solicitudes);
         setAllSolicitudes(solicitudes);
         setTipoCounts(tipoCounts);
-
       } else {
-
         const userData = checkAuthToken();
-        const solicitudesData = await getRequests(userData?.email, rowsPerPage, currentPage);
-
+        const solicitudesData = await getRequests(
+          userData?.email,
+          rowsPerPage,
+          currentPage
+        );
 
         const {
           allSolicitudes = [],
           tipoCounts,
           statusCounts,
-          months
+          months,
         } = solicitudesData;
 
         setAllSolicitudes(allSolicitudes);
@@ -182,9 +193,8 @@ const LegixStatistics: React.FC = () => {
         setStatusCounts(statusCounts);
         setMonths(months);
       }
-
     } catch (error) {
-      console.error('Failed to fetch all solicitudes:', error);
+      console.error("Failed to fetch all solicitudes:", error);
     }
   };
 
@@ -193,14 +203,22 @@ const LegixStatistics: React.FC = () => {
       let solicitudesData;
 
       if (
-        (typeof formData.rol === 'number' && formData.rol < 20) ||
-        (typeof formData.rol === 'string' && (formData.rol === 'Cliente' || formData.rol === 'Cliente recurrente'))
+        (typeof formData.rol === "number" && formData.rol < 20) ||
+        (typeof formData.rol === "string" &&
+          (formData.rol === "Cliente" || formData.rol === "Cliente recurrente"))
       ) {
-        solicitudesData = await getRequestsCuenta(rowsPerPage, formData.cuenta, lastVisibleCursor);
+        solicitudesData = await getRequestsCuenta(
+          rowsPerPage,
+          formData.cuenta,
+          lastVisibleCursor
+        );
       } else {
         const userData = checkAuthToken();
-        solicitudesData = await getRequests(userData?.email, rowsPerPage, currentPage)
-
+        solicitudesData = await getRequests(
+          userData?.email,
+          rowsPerPage,
+          currentPage
+        );
       }
 
       const { solicitudes, pagination } = solicitudesData;
@@ -208,9 +226,8 @@ const LegixStatistics: React.FC = () => {
       setSolicitudes(solicitudes);
       setLastVisibleCursor(pagination.nextCursor || null);
       setHasNextPage(pagination.hasNextPage);
-
     } catch (error) {
-      console.error('Failed to fetch paginated solicitudes:', error);
+      console.error("Failed to fetch paginated solicitudes:", error);
     }
   };
 
@@ -239,7 +256,7 @@ const LegixStatistics: React.FC = () => {
     "new-sociedad-empresa": "Sociedad / Empresa",
     "menores-al-extranjero": "Salida de Menores al Extranjero",
     "pension-alimenticia": "Pensión Alimenticia",
-    "pension": "Pensión Alimenticia",
+    pension: "Pensión Alimenticia",
     "tramite-general": "Trámite General",
     "pension-desacato": "Pensión Desacato",
     "solicitud-cliente-recurrente": "Solicitud Cliente Recurrente",
@@ -272,12 +289,10 @@ const LegixStatistics: React.FC = () => {
     (solicitud) => parseInt(solicitud.status) === 70
   ).length;
 
-  const solicitudEnProceso = solicitudesFiltradas.filter(
-    (solicitud) => {
-      const status = parseInt(solicitud.status);
-      return status !== 70 && status !== 1; 
-    }
-  ).length;
+  const solicitudEnProceso = solicitudesFiltradas.filter((solicitud) => {
+    const status = parseInt(solicitud.status);
+    return status !== 70 && status !== 1;
+  }).length;
 
   const solicitudesFinalizadas = getSolicitudesFiltradasPorRol(allSolicitudes)
     .filter((solicitud) => parseInt(solicitud.status) === 70)
@@ -307,7 +322,8 @@ const LegixStatistics: React.FC = () => {
 
   const tipoCountsFiltrados: { [key: string]: number } = {};
   solicitudesFiltradas.forEach((solicitud) => {
-    tipoCountsFiltrados[solicitud.tipo] = (tipoCountsFiltrados[solicitud.tipo] || 0) + 1;
+    tipoCountsFiltrados[solicitud.tipo] =
+      (tipoCountsFiltrados[solicitud.tipo] || 0) + 1;
   });
 
   // Paginación de solicitudes finalizadas
@@ -326,13 +342,17 @@ const LegixStatistics: React.FC = () => {
   useEffect(() => {
     setPaginationEnProceso({
       hasPrevPage: currentPageEnProceso > 1,
-      hasNextPage: currentPageEnProceso < Math.ceil(solicitudesEnProceso.length / rowsPerPage),
+      hasNextPage:
+        currentPageEnProceso <
+        Math.ceil(solicitudesEnProceso.length / rowsPerPage),
       totalPages: Math.ceil(solicitudesEnProceso.length / rowsPerPage),
     });
 
     setPaginationFinalizadas({
       hasPrevPage: currentPageFinalizadas > 1,
-      hasNextPage: currentPageFinalizadas < Math.ceil(solicitudesFinalizadas.length / rowsPerPage),
+      hasNextPage:
+        currentPageFinalizadas <
+        Math.ceil(solicitudesFinalizadas.length / rowsPerPage),
       totalPages: Math.ceil(solicitudesFinalizadas.length / rowsPerPage),
     });
   }, [
@@ -340,9 +360,8 @@ const LegixStatistics: React.FC = () => {
     solicitudesFinalizadas.length,
     currentPageEnProceso,
     currentPageFinalizadas,
-    rowsPerPage
+    rowsPerPage,
   ]);
-
 
   useEffect(() => {
     if (formData.cuenta) {
@@ -351,32 +370,86 @@ const LegixStatistics: React.FC = () => {
   }, [currentPage]);
 
   useEffect(() => {
-    console.log("🚀 ~ tipoCounts:", tipoCounts)
+    console.log("🚀 ~ tipoCounts:", tipoCounts);
   }, [tipoCounts]);
-
 
   return (
     <div className="flex flex-col gap-4 p-8 w-full">
       <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2 w-[97%]">
-        <HomeBox title="Sociedades" number={tipoCountsFiltrados['new-sociedad-empresa'] || 0} color="bg-[#9694FF]" />
+        <HomeBox
+          title="Sociedades"
+          number={tipoCountsFiltrados["new-sociedad-empresa"] || 0}
+          color="bg-[#9694FF]"
+        />
         <HomeBox
           title="Fundaciones"
-          number={(tipoCountsFiltrados['new-fundacion-interes-privado'] || 0) + (tipoCountsFiltrados['new-fundacion'] || 0)}
+          number={
+            (tipoCountsFiltrados["new-fundacion-interes-privado"] || 0) +
+            (tipoCountsFiltrados["new-fundacion"] || 0)
+          }
           color="bg-[#57caeb]"
         />
         <HomeBox
           title="Propuesta Legal"
-          number={(tipoCountsFiltrados['Propuesta-Legal'] || 0) + (tipoCountsFiltrados['consulta-legal'] || 0) + (tipoCountsFiltrados['propuesta-legal'] || 0)}
+          number={
+            (tipoCountsFiltrados["Propuesta-Legal"] || 0) +
+            (tipoCountsFiltrados["consulta-legal"] || 0) +
+            (tipoCountsFiltrados["propuesta-legal"] || 0)
+          }
           color="bg-[#5ddab4]"
         />
-        <HomeBox title="Consulta Escrita" number={(tipoCountsFiltrados['Consulta-Escrita'] || 0) + (tipoCountsFiltrados['consulta-escrita'] || 0)} color="bg-[#ff7976]" />
-        <HomeBox title="Consulta Virtual" number={(tipoCountsFiltrados['Consulta-Virtual'] || 0) + (tipoCountsFiltrados['consulta-virtual'] || 0)} color="bg-black" />
-        <HomeBox title="Consulta Presencial" number={(tipoCountsFiltrados['Consulta-Presencial'] || 0) + (tipoCountsFiltrados['consulta-presencial'] || 0)} color="bg-[#f4a261]" />
-        <HomeBox title="Tramite General" number={tipoCountsFiltrados['tramite-general'] || 0} color="bg-[#e76f51]" />
-        <HomeBox title="Pensiones" number={(tipoCountsFiltrados['pension-alimenticia'] || 0) + (tipoCountsFiltrados['pension'] || 0)} color="bg-[#2a9d8f]" />
-        <HomeBox title="Pension Desacato" number={tipoCountsFiltrados['pension-desacato'] || 0} color="bg-[#264653]" />
-        <HomeBox title="Salida de Menores al Extranjero" number={tipoCountsFiltrados['menores-al-extranjero'] || 0} color="bg-[#e9c46a]" />
-        <HomeBox title="Cliente Recurrente" number={tipoCountsFiltrados['solicitud-cliente-recurrente'] || 0} color="bg-[#264653]" />
+        <HomeBox
+          title="Consulta Escrita"
+          number={
+            (tipoCountsFiltrados["Consulta-Escrita"] || 0) +
+            (tipoCountsFiltrados["consulta-escrita"] || 0)
+          }
+          color="bg-[#ff7976]"
+        />
+        <HomeBox
+          title="Consulta Virtual"
+          number={
+            (tipoCountsFiltrados["Consulta-Virtual"] || 0) +
+            (tipoCountsFiltrados["consulta-virtual"] || 0)
+          }
+          color="bg-black"
+        />
+        <HomeBox
+          title="Consulta Presencial"
+          number={
+            (tipoCountsFiltrados["Consulta-Presencial"] || 0) +
+            (tipoCountsFiltrados["consulta-presencial"] || 0)
+          }
+          color="bg-[#f4a261]"
+        />
+        <HomeBox
+          title="Tramite General"
+          number={tipoCountsFiltrados["tramite-general"] || 0}
+          color="bg-[#e76f51]"
+        />
+        <HomeBox
+          title="Pensiones"
+          number={
+            (tipoCountsFiltrados["pension-alimenticia"] || 0) +
+            (tipoCountsFiltrados["pension"] || 0)
+          }
+          color="bg-[#2a9d8f]"
+        />
+        <HomeBox
+          title="Pension Desacato"
+          number={tipoCountsFiltrados["pension-desacato"] || 0}
+          color="bg-[#264653]"
+        />
+        <HomeBox
+          title="Salida de Menores al Extranjero"
+          number={tipoCountsFiltrados["menores-al-extranjero"] || 0}
+          color="bg-[#e9c46a]"
+        />
+        <HomeBox
+          title="Cliente Recurrente"
+          number={tipoCountsFiltrados["solicitud-cliente-recurrente"] || 0}
+          color="bg-[#264653]"
+        />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 w-full">
         <div className="lg:col-span-2">
@@ -403,24 +476,31 @@ const LegixStatistics: React.FC = () => {
           />
 
           {!(
-            (typeof formData.rol === 'number' && formData.rol < 50) ||
-            (typeof formData.rol === 'string' && formData.rol === 'Cliente') ||
-            (typeof formData.rol === 'string' && formData.rol === 'Cliente recurrente')
+            (typeof formData.rol === "number" && formData.rol < 50) ||
+            (typeof formData.rol === "string" && formData.rol === "Cliente") ||
+            (typeof formData.rol === "string" &&
+              formData.rol === "Cliente recurrente")
           ) && <PivotTable months={months} />}
-
         </div>
         <div className="lg:col-span-1">
           {!(
-            (typeof formData.rol === 'number' && formData.rol < 50) ||
-            (typeof formData.rol === 'string' && formData.rol === 'Cliente') ||
-            (typeof formData.rol === 'string' && formData.rol === 'Cliente recurrente')
-          ) &&
+            (typeof formData.rol === "number" && formData.rol < 50) ||
+            (typeof formData.rol === "string" && formData.rol === "Cliente") ||
+            (typeof formData.rol === "string" &&
+              formData.rol === "Cliente recurrente")
+          ) && (
             <>
-              <DashboardCard title={"Solicitudes pendientes de pago"} value={statusCounts.status10} />
-              <DashboardCard title={"Solicitudes pagadas"} value={statusCounts.status20} />
+              <DashboardCard
+                title={"Solicitudes pendientes de pago"}
+                value={statusCounts.status10}
+              />
+              <DashboardCard
+                title={"Solicitudes pagadas"}
+                value={statusCounts.status20}
+              />
               <DashboardCard title={"Balance de ingresos"} value={0.0} />
             </>
-          }
+          )}
           <FormalitiesChart
             solicitudFinalizada={solicitudFinalizada}
             solicitudEnProceso={solicitudEnProceso}
