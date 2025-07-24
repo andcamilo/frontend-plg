51c30b36147bc087ecb80d8432a74d8203294abe
Felipe F <mundofelipote@gmail.com>
2025-07-24 15:50:29 -0300

    Implement useAllSolicitudes hook to centralize data fetching for LegixStatistics; update LegixStadisticsContainer to handle new props and loading/error states, enhancing overall data management and component structure.

---
+++ src/app/dashboard/home/hooks/useAllSolicitudes.query.ts
@@ -0,0 +1,49 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { fetchUser } from "../services/request-user-cuenta.service";
+import { checkAuthToken } from "@/src/app/utils/checkAuthToken";
+import { getRequestsCuenta } from "../services/request-cuenta.service";
+import { getRequests } from "../services/requests-by-email.service";
+
+export const useAllSolicitudes = (pagination: any) => {
+  return useQuery({
+    queryKey: ["allSolicitudes"],
+    queryFn: async () => {
+      const userData = checkAuthToken();
+      if (!userData?.user_id) {
+        throw new Error("User ID not found");
+      }
+      const userCuenta = await fetchUser(userData.user_id);
+      const rol = userCuenta.rol;
+      const rowsPerPage = 10;
+      const currentPage = 1;
+      const lastVisibleCursor = pagination.nextCursor || null;
+
+
+      let solicitudesData;
+      if (
+        (typeof rol === "number" && rol < 20) ||
+        (typeof rol === "string" &&
+          (rol === "Cliente" || rol === "Cliente recurrente"))
+      ) {
+        solicitudesData = await getRequestsCuenta(
+          rowsPerPage,
+          userData.user_id,
+          lastVisibleCursor
+        );
+      } else {
+        solicitudesData = await getRequests(
+          userData.email,
+          rowsPerPage,
+          currentPage
+        );
+      }
+      const { allSolicitudes = [], statusCounts, months } = solicitudesData;
+      return {
+        allSolicitudes,
+        statusCounts,
+        months,
+      };
+    },
+  });
+};
