6b797a779bf5aa0ce0b73a5eb965d3048cde3f7b
Felipe F <mundofelipote@gmail.com>
2025-07-28 17:57:49 -0300

    Add alert management functionality with new AlertForm and AlertFormEdit components; implement useAlertsQuery and associated service methods for CRUD operations on alerts, utilizing react-hook-form and zod for validation.

---
+++ src/app/dashboard/home/hooks/Alerts/useAlerts.query.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  createAlert,
+  deleteAlert,
+  getAlertById,
+  updateAlert,
+} from "../../services/Alerts/alerts.service";
+import { AlertsSchema } from "../../schemas/alerts.schema";
+
+export const useAlertsQuery = (id: string) => {
+  return useQuery({
+    queryKey: ["alerts", id],
+    queryFn: () => getAlertById(id),
+  });
+};
+
+export const useCreateAlertMutation = () => {
+  return useMutation({
+    mutationFn: createAlert,
+  });
+};
+
+export const useUpdateAlertMutation = (id: string) => {
+  return useMutation({
+    mutationFn: (alert: AlertsSchema) => updateAlert(id, alert),
+  });
+};
+
+export const useDeleteAlertMutation = (id: string) => {
+  return useMutation({
+    mutationFn: deleteAlert,
+  });
+};
---
+++ src/app/dashboard/home/services/Alerts/alerts.service.ts
@@ -0,0 +1,50 @@
+import { AlertsSchema } from "../../schemas/alerts.schema";
+import { backendBaseUrl, backendEnv } from "@utils/env";
+
+export const getAlertById = async (id: string) => {
+  const response = await fetch(
+    `${backendBaseUrl}/${backendEnv}/get-alert/${id}`
+  );
+  if (!response.ok) {
+    throw new Error(`Error fetching alerts: ${response.statusText}`);
+  }
+  return response.json();
+};
+
+export const createAlert = async (alert: AlertsSchema) => {
+  const response = await fetch(`${backendBaseUrl}/${backendEnv}/create-alert`, {
+    method: "POST",
+    body: JSON.stringify(alert),
+  });
+  if (!response.ok) {
+    throw new Error(`Error creating alert: ${response.statusText}`);
+  }
+  return response.json();
+};
+
+export const updateAlert = async (id: string, alert: AlertsSchema) => {
+  const response = await fetch(
+    `${backendBaseUrl}/${backendEnv}/update-alert/${id}`,
+    {
+      method: "PUT",
+      body: JSON.stringify(alert),
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Error updating alert: ${response.statusText}`);
+  }
+  return response.json();
+};
+
+export const deleteAlert = async (id: string) => {
+  const response = await fetch(
+    `${backendBaseUrl}/${backendEnv}/delete-alert/${id}`,
+    {
+      method: "DELETE",
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Error deleting alert: ${response.statusText}`);
+  }
+  return response.json();
+};
