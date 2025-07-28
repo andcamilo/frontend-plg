6642d0b3aff9b1c25164b1ea9b68a35479535f33
Felipe F <mundofelipote@gmail.com>
2025-07-28 18:22:20 -0300

    Refactor Requests component to Home; integrate AlertFormEdit and AlertFormCreate modals for enhanced alert management functionality.

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import LegixStadisticsContainer from "./components/LegixStadisticsContainer";
 import TableRequestsContainer from "./components/TableRequests/TableRequestsContainer";
+import { Modal } from "@app/(global)/components/Modal";
+import AlertFormEdit from "./components/Alerts/AlertFormEdit";
+import AlertFormCreate from "./components/Alerts/AlertFormCreate";
 
-export default function Requests() {
+export default function Home() {
   return (
     <div className="p-4">
       <h1 className="text-4xl font-bold text-white pl-8 mb-4">
@@ -10,6 +13,12 @@ export default function Requests() {
       </h1>
       <TableRequestsContainer />
       <LegixStadisticsContainer />
+      <Modal modalId="alert-form-edit">
+        <AlertFormEdit />
+      </Modal>
+      <Modal modalId="alert-form-create">
+        <AlertFormCreate />
+      </Modal>
     </div>
   );
 }
