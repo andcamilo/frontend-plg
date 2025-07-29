195e46bf388f9770bce56245c14387f57e219642
cristian-delgadillo <pujols.cmt@gmail.com>
2025-07-29 14:55:52 -0500

    comienzo del desarrollo de la solicitud Acta Sociedad Fundacion

---
+++ src/app/request/acta-sociedadFundacion/[id]/page.tsx
@@ -0,0 +1,7 @@
+import ActaSociedadFundacionBase from '../ActaSociedadFundacionBase';
+
+const ActaSociendadFundacion: React.FC = () => {
+    return <ActaSociedadFundacionBase />;
+};
+
+export default ActaSociendadFundacion;
