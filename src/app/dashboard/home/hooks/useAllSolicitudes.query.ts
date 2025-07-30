5998013231468f61a804f13080e352ad8fb74167
Felipe F <mundofelipote@gmail.com>
2025-07-30 15:33:54 -0300

    Refactor TableRequests and TableRequestsContainer components to remove lawyer filtering functionality. Simplify data handling by directly passing solicitudes to TableRequests and updating the useAllSolicitudes hook to fetch data without pagination. Introduce new solicitudes service for improved data retrieval.

--- src/app/dashboard/home/hooks/useAllSolicitudes.query.ts
+++ src/app/dashboard/home/hooks/useAllSolicitudes.query.ts
@@ -1,44 +1,15 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import { getRequestsCuenta } from "../services/request-cuenta.service";
-import { getRequests } from "../services/requests-by-email.service";
-import { useUserCuenta } from "./useUserCuenta.query";
+import { getSolicitudes } from "../services/solicitudes.service";
+import { decodeUserToken } from "@app/(global)/utils/decode-user-token.util";
 
-export const useAllSolicitudes = (pagination: any) => {
-  const { data: userCuenta } = useUserCuenta();
-
+export const useAllSolicitudes = () => {
   return useQuery({
     queryKey: ["allSolicitudes"],
     queryFn: async () => {
-      const rol = userCuenta?.rol;
-      const rowsPerPage = 10;
-      const currentPage = 1;
-      const lastVisibleCursor = pagination.nextCursor || null;
-
-      let solicitudesData;
-      if (
-        (typeof rol === "number" && rol < 20) ||
-        (typeof rol === "string" &&
-          (rol === "Cliente" || rol === "Cliente recurrente"))
-      ) {
-        solicitudesData = await getRequestsCuenta(
-          rowsPerPage,
-          userCuenta?.id ?? "",
-          lastVisibleCursor
-        );
-      } else {
-        solicitudesData = await getRequests(
-          userCuenta?.email ?? "",
-          rowsPerPage,
-          currentPage
-        );
-      }
-      const { allSolicitudes = [], statusCounts, months } = solicitudesData;
-      return {
-        allSolicitudes,
-        statusCounts,
-        months,
-      };
+      const userData = decodeUserToken();
+      const solicitudesData = await getSolicitudes(userData?.user_id ?? "");
+      return solicitudesData.solicitudes;
     },
   });
 };
