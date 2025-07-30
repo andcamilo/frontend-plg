9f8e924c5f1c8d2923c37328ad54b092278c5501
Felipe F <mundofelipote@gmail.com>
2025-07-30 12:10:00 -0300

    Update user-cuenta adapter and types to include additional user properties for enhanced data structure and retrieval. Add fields for id, uid, cuenta, date, email, nombre, cedulaPasaporte, status, and telefonoSolicita to improve user information handling.

--- src/app/dashboard/home/types/user-cuenta.types.ts
+++ src/app/dashboard/home/types/user-cuenta.types.ts
@@ -1,3 +1,15 @@
 export interface User {
+  id: string;
+  uid: string;
+  cuenta: string;
+  date: {
+    _seconds: number;
+    _nanoseconds: number;
+  };
+  email: string;
+  nombre: string;
+  cedulaPasaporte: string;
   rol: string | number;
+  status: number;
+  telefonoSolicita: string;
 }
