20a6aab90d6baf407d757d7d1c517bf8e2f5ae27
Felipe F <mundofelipote@gmail.com>
2025-07-31 12:19:03 -0300

    Add TableRequestsSkeleton component for loading state and update TableRequestsContainer to use it, enhancing user experience during data fetching.

--- src/app/dashboard/home/components/TableRequests/TableRequestsContainer.tsx
+++ src/app/dashboard/home/components/TableRequests/TableRequestsContainer.tsx
@@ -2,11 +2,12 @@
 
 import { useAllSolicitudes } from "../../hooks/useAllSolicitudes.query";
 import TableRequests from "./TableRequests";
+import TableRequestsSkeleton from "./TableRequestsSkeleton";
 
 const TableRequestsContainer = () => {
   const { data: solicitudes, isLoading, isError } = useAllSolicitudes();
 
-  if (isLoading) return <div>Cargando...</div>;
+  if (isLoading) return <TableRequestsSkeleton />;
   if (isError) return <div>Error al cargar las solicitudes</div>;
   if (solicitudes?.length === 0) return <div>No hay solicitudes</div>;
 
