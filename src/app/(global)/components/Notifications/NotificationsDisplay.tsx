782ed39bf99af5aae94649fc1ee713f697c9540f
Felipe F <mundofelipote@gmail.com>
2025-08-07 14:28:51 -0300

    Refactor NotificationsDisplay component to wrap alert items in Link for navigation; enhance layout with flexbox and spacing adjustments for improved user experience.

--- src/app/(global)/components/Notifications/NotificationsDisplay.tsx
+++ src/app/(global)/components/Notifications/NotificationsDisplay.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const NotificationsDisplay = ({ notifications }: { notifications: any }) => {
   if (!notifications)
     return <div className="p-4 text-gray-500">No hay notificaciones</div>;
@@ -18,29 +20,28 @@ const NotificationsDisplay = ({ notifications }: { notifications: any }) => {
           {overdueAlerts.length} alerta(s) listada(s)
         </span>
       </div>
-      <div className="space-y-4">
+      <div className="space-y-4 gap-2 mt-2 flex flex-col">
         {overdueAlerts.map((alert: any) => (
-          <div
-            key={alert.id}
-            className="rounded-md border border-gray-200 bg-gray-800 p-3 shadow-sm"
-          >
-            <div className="font-semibold text-white mb-1">
-              Expediente: {alert.expediente || alert.solicitudId}
+          <Link href={`/dashboard/request/${alert.solicitudId}`} key={alert.id}>
+            <div className="rounded-md border border-gray-200 bg-gray-800 p-3 shadow-sm">
+              <div className="font-semibold text-white mb-1">
+                Expediente: {alert.expediente || alert.solicitudId}
+              </div>
+              <div className="text-xs text-white mb-2">
+                Solicitante: {alert.nombreSolicita || alert.email}
+              </div>
+              <div className="bg-red-50 border-l-4 border-red-400 px-2 py-1 text-xs text-red-800">
+                <span className="font-bold">
+                  {alert.reminderText || alert.title || "Alerta vencida"}
+                </span>
+                <br />
+                <span>
+                  <span className="font-semibold">Vencida:</span>{" "}
+                  {new Date(alert.reminderDateTime).toLocaleString()}
+                </span>
+              </div>
             </div>
-            <div className="text-xs text-white mb-2">
-              Solicitante: {alert.nombreSolicita || alert.email}
-            </div>
-            <div className="bg-red-50 border-l-4 border-red-400 px-2 py-1 text-xs text-red-800">
-              <span className="font-bold">
-                {alert.reminderText || alert.title || "Alerta vencida"}
-              </span>
-              <br />
-              <span>
-                <span className="font-semibold">Vencida:</span>{" "}
-                {new Date(alert.reminderDateTime).toLocaleString()}
-              </span>
-            </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
